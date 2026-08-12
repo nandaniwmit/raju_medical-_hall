370da383187af01f06e78025c99933538ca26bbd
nandaniwmit <nandani.wmit@gmail.com>
2026-08-12 20:22:43 +0530

    Update tracking endpoint in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Cross, MapPin, Phone, Mail, Clock, MessageCircle, ExternalLink, ShieldC
 
 export default function Footer() {
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
